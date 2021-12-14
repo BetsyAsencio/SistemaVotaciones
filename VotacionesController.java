4e2353d1dc11ac5c47f5d32d92be608c32d72615
BetsyAsencio <96151978+BetsyAsencio@users.noreply.github.com>
2021-12-14 17:46:08 -0600

    Add files via upload

---
+++ VotacionesController.java
@@ -0,0 +1,42 @@
+
+package Vistas;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+public class VotacionesController implements Initializable {
+
+    @FXML
+    private TextField TxtYavotado;
+    @FXML
+    private Button btVotar1;
+    @FXML
+    private Button btVotar2;
+    @FXML
+    private Button btVotar3;
+
+   
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+       
+    }    
+
+    @FXML
+    private void Votar1(ActionEvent event) {
+    }
+
+    @FXML
+    private void Votar2(ActionEvent event) {
+    }
+
+    @FXML
+    private void Votar3(ActionEvent event) {
+    }
+    
+}
